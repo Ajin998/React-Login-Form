4bf94021774df1b8e8061fd6fb721fd80f055ed2
Ajin Varghese <ajin.varghese@contentstack.com>
2021-01-09 14:34:30 +0530

    Signup completed
    So far so good, when user signedup successfully then an alert will be displayed to confirm for redirecting to login page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { signup } from "../../constants/endpoints";
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 import styles from "./Register.module.scss";
 function Register() {
+  const [error, setError] = useState();
+  const [status, setStatus] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
+
+  let redirect;
   function handleClick() {
     alert("You are already in Register page");
   }
@@ -18,6 +23,26 @@ function Register() {
   function checkForConfirmPassword(e) {
     setConfirmPassword(e.target.value);
   }
+  function signMeUp(e) {
+    e.preventDefault();
+    let formData = { email, password, confirmPassword };
+    let formObject = JSON.stringify(formData);
+    fetch(signup, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: formObject,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "Unsuccessfull") return setError(data.message);
+        setStatus(data.status);
+      })
+      .catch((err) => {
+        console.log("Kuch tho fatta:", err);
+      });
+  }
   return (
     <div className={styles["Loginpage"]}>
       <h1 onClick={handleClick}>Register Page</h1>
@@ -26,9 +51,26 @@ function Register() {
           Back to Login
         </Link>
       </button>
-      <form method="post" className={styles["form__section"]}>
+      {/* <NotificationContainer /> */}
+      {/* if error is true then display error */}
+      {error && <p className={styles["error__msg"]}>{error}</p>}
+      {status && (
+        <>
+        {redirect= window.confirm("Signed Up\nPress OK to Go in Login Page")}
+          {/* <p className={styles["status__msg"]}>{status}</p> */}
+          {redirect?<Redirect to="/" />: <Redirect to="/register" />}
+        </>
+      )}
+      {/* {status && <p className={styles["status__msg"]}>{status}</p>} */}
+      <form
+        method="post"
+        onSubmit={signMeUp}
+        name="signup__form"
+        encType="application/x-www-form-urlencoded"
+        className={styles["form__section"]}
+      >
         <div className={styles["email__section"]}>
-          <label htmlFor="Email">
+          <label htmlFor="Email" name="email">
             Email<sup>*</sup>{" "}
           </label>
           <input
@@ -41,7 +83,7 @@ function Register() {
           />
         </div>
         <div className={styles["password__section"]}>
-          <label htmlFor="Password">
+          <label htmlFor="Password" name="password">
             Password<sup>*</sup>{" "}
           </label>
           <input
@@ -54,7 +96,7 @@ function Register() {
           />
         </div>
         <div className={styles["password__section"]}>
-          <label htmlFor="Password">
+          <label htmlFor="Password" name="confirmPassword">
             {" "}
             Confirm-Password<sup>*</sup>{" "}
           </label>
